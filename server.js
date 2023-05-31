ff8851ea009c6d044aae23a7bd12a444a66eea51
Johan Smit <johansm@bbd.co.za>
2023-05-31 19:00:30 +0200

    s

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
 
-var port = process.env.PORT;
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
-
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
+
+app.get('/test', function(req, res) {
+  res.status(200).send("Hey welcome");
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
